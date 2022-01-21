fce89a1e011278d87b4a51a58e06a55df12dca44
alexandertarakanov <alex.artoff@gmail.com>
2022-01-21 21:10:21 +0500

    add cli.py; modefy brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
+from brain_games.cli import welcome_user
+
 def welcome():
-    print("Welcome to the Brain Games!")
+	print("Welcome to the Brain Games!")
 
 def main():
-    welcome()
+	welcome()
+	welcome_user()
 
 if __name__ == "__main__":
-    main()
+	main()
